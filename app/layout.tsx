6b4bd51d2b25ab53068107f6e6f671c8cfcb4cc0
Berlin <evan1234.ek@gmail.com>
2024-02-24 00:17:14 +0600

    Update fonts and metadata in layout component, add assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,30 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from 'next/font/google';
 import React from 'react';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-space-grotesk',
+});
 
 export const metadata: Metadata = {
-  title: 'Stack Overflow',
+  title: 'Dev Overflow',
   description:
-    'Stack Overflow is the largest, most trusted online community for developers to learn, share their knowledge, and build their careers.',
+    'Dev Overflow is a community of developers, for developers, by developers. We are dedicated to providing a safe, welcoming, and inclusive space for all developers.',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  },
 };
 
 export default function RootLayout({
@@ -18,9 +33,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500',
+        },
+      }}
+    >
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
