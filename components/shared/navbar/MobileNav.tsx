0981d8dccf47db85d3811991fd799a47716a7a7d
Berlin <evan1234.ek@gmail.com>
2024-02-25 12:58:49 +0600

    Add LeftsideBar component and update MobileNav styling

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -65,7 +65,7 @@ export default function MobileNav() {
       </SheetTrigger>
       <SheetContent
         side="left"
-        className="background-light900_dark200 border-none"
+        className="background-light900_dark200 overflow-y-auto border-none"
       >
         <Link href="/" className="flex items-center gap-1">
           <Image
