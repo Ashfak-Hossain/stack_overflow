d3537adb2d094e5d9df19a9ebb3e3d565c01d988
Berlin <evan1234.ek@gmail.com>
2024-02-27 23:03:34 +0600

    Refactor user ID retrieval in ask-question page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -4,9 +4,7 @@ import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
 export default async function Page() {
-  // const { userId } = auth();
-
-  const userId = 'dummyClerkId';
+  const { userId } = auth();
 
   if (!userId) redirect('/sign-in');
 
