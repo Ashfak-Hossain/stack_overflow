ba32828c2575ff72d8f5b2449ffcbaf06925c118
Berlin <evan1234.ek@gmail.com>
2024-07-14 00:12:27 +0600

    chore: navbar implementation

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
 
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
-
 const Home = () => {
-  return (
-    <div className="flex flex-col">
-      <div className="">
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </div>
-      Home
-    </div>
-  );
+  return <div className="flex flex-col">Home</div>;
 };
 
 export default Home;
